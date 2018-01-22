1091d2d32f80b589444bc44783b1d7dfdabd1f24
ssellai <31967188+ssellai@users.noreply.github.com>
2018-01-22 11:26:23 -0500

    fixed divisor logic

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def students_per_group(my_list, n): #n is the number of students per group desir
     return [my_list[x:x+n] for x in range(0, len(my_list), n)]
 
 def number_of_groups(my_list, n): #n is the number of groups desired,
-    d = (len(my_list)//n)
+    d = math.ceil((len(my_list)/n))
     return students_per_group(my_list, d)

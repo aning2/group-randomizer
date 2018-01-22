d7a08485213d89f2933c676b664fcffce80f4d9c
ssellai <31967188+ssellai@users.noreply.github.com>
2018-01-22 14:06:04 -0500

    added test output

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 wb = openpyxl.load_workbook('class_list.xlsx')
 
 
-'''ws = wb.active
+ws = wb.active
 first_column = ws['B']
 class_list = []
 
 for i in range(len(first_column)):
     class_list.append(first_column[i].value)
     #print(first_column[i].value)
-    random.shuffle(class_list)'''
+    random.shuffle(class_list)
 
 def students_per_group(my_list, n): #n is the number of students per group desired
     return [my_list[x:x+n] for x in range(0, len(my_list), n)]
@@ -21,3 +21,6 @@ def students_per_group(my_list, n): #n is the number of students per group desir
 def number_of_groups(my_list, n): #n is the number of groups desired,
     d = (len(my_list)+n-1) // n
     return students_per_group(my_list, d)
+
+print(number_of_groups(class_list, 5))
+print(students_per_group(class_list, 4))

0867251c3523bad0df0ae2b7dd8f2c07619be6c7
ssellai <31967188+ssellai@users.noreply.github.com>
2018-01-22 18:15:45 -0500

    finalized product

--- app.py
+++ app.py
@@ -47,23 +47,12 @@ def number_of_groups(n, l):  # n is the number of groups desired,
     d = (len(l) + n - 1) // n
     return students_per_group(d, l)
 
-
-def students_per_group(n, l):  # n is the number of students per group desired
-    return [l[x:x + n] for x in range(0, len(l), n)]
-
+def students_per_group(n, l): #n is the number of students per group desired
+    return [l[x:x+n] for x in range(0, len(l), n)]
 
 def clean_list(l):
-    return [name for name in l if name not in ['', '\r', '\n', '\r\n']]
-
+	return [name for name in l if name not in ['', '\r', '\n', '\r\n']]
 
 def clean_name(name):
-    temp = name.split(',')
-    return ' '.join(reversed(temp))
-
-
-def listToString(l):
-    s = ""
-    for i in list:
-        s += (i + '\n')
-
-    return s
+	temp = name.split(',')
+	return ' '.join(reversed(temp))

654aadcfa11193af793d82787d35c542e039b9a8
ssellai <31967188+ssellai@users.noreply.github.com>
2018-01-22 09:28:39 -0500

    created and randomized class list

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ wb = openpyxl.load_workbook('class_list.xlsx')
 
 ws = wb.active
 first_column = ws['B']
+class_list = []
+
+for i in range(len(first_column)):
+    class_list.append(first_column[i].value)
+    #print(first_column[i].value)
+    random.shuffle(class_list)
+    

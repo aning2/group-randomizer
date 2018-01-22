80841c6b77619b5c75a9cb87a4d93ada227971c8
ssellai <31967188+ssellai@users.noreply.github.com>
2018-01-22 15:50:49 -0500

    cleaned up code and changed variables

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import random
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-wb = openpyxl.load_workbook('class_list.xlsx')
+wb = openpyxl.load_workbook(filename='class_list.xlsx')
+
 
 @app.route('/period1')
 def index():
@@ -32,28 +33,24 @@ def my_form_post():
 
     return render_template('index.html', groups=groups)
 
-'''ws = wb.active
-first_column = ws['B']
-class_list = []
 
-for i in range(len(first_column)):
-    class_list.append(first_column[i].value)
-    #print(first_column[i].value)
-    random.shuffle(class_list)'''
+def number_of_groups(n, l):  # n is the number of groups desired,
+    d = (len(l) // n)
+    return students_per_group(d, l)
 
-def students_per_group(my_list, n): #n is the number of students per group desired
-    return [my_list[x:x+n] for x in range(0, len(my_list), n)]
 
-def number_of_groups(my_list, n): #n is the number of groups desired,
-    d = (len(my_list)+n-1) // n
-    return students_per_group(my_list, d)
+def students_per_group(n, l):  # n is the number of students per group desired
+    return [l[x:x + n] for x in range(0, len(l), n)]
+
 
 def clean_list(l):
-        return [name for name in l if name not in ['', '\r', '\n', '\r\n']]
+    return [name for name in l if name not in ['', '\r', '\n', '\r\n']]
+
 
 def clean_name(name):
     temp = name.split(',')
     return ' '.join(reversed(temp))
+
 
 def listToString(l):
     s = ""
